a690ecfbb56dde35d603099d3cbe0787a2637e57
silviarigatti <92572878+silviarigatti@users.noreply.github.com>
2021-11-19 11:01:41 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -2,5 +2,21 @@
 
 install.packages("raster")
 library(raster)
+install.packages("rgdal")
+library(rgdal)
 
 # we have to set the working directory
+setwd("C:/lab/")
+
+# let's import the data
+l1992 <- brick("defor1_.jpg") # image of 1992
+
+# let's see the information of the data
+l1992
+
+# Bands: defor_.1, defor_.2, defor_.1
+# plotRGB
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+plotRGB(l1992, r=2, g=3, b=1, stretch="Lin")
+
+
