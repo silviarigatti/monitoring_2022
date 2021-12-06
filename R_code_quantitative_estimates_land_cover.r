ba342bca9721570c41df12634952ac9da1276618
silviarigatti <92572878+silviarigatti@users.noreply.github.com>
2021-12-06 10:55:13 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -3,6 +3,7 @@
 library(raster)
 library(RStoolbox)
 library(ggplot2)
+library(gridExtra)
 setwd("C:/lab/")
 
 # brick
@@ -20,15 +21,15 @@ plot(list_rast[[1]])
 # defor: NIR1, red 2, green 3
 plotRGB(list_rast[[1]], r=1, g=2, b=3, stretch="lin")
 # let's assign an easier name to the image
-l1996 <- list_rast[[1]]
-plotRGB(l1996, r=1, g=2, b=3, stretch="lin")
+l1992 <- list_rast[[1]]
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 
 # let's do the same with the 2006 image
 l2006 <- list_rast[[2]]
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 # let's estimate the amount of forest loss
-# Unsupervised classification
+# Unsupervised classification: now we classify the pixels
 l1992 <- list_rast[[1]]
 l1992c <- unsuperClass(l1992, nClasses=2)
 l1992c
@@ -59,4 +60,56 @@ proportion1992 <- data.frame(cover, prop1992)
 proportion1992
 
 # let's make a plot, an histogram
+# to use this function, the ggplot package is requested. we installed it at the beginning of the code
 ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white") 
+# aes in the previous code stays for aesthetics
+
+# 06.12
+
+l2006 <- list_rast[[2]]
+l2006c <- unsuperClass(l2006, nClasses=2)
+l2006c
+
+# let's obtain an image with 2 values: 1 and 2.
+plot(l2006c$map)
+# value 1 = agricultural areas and water
+# value 2 = forest
+
+# how many pixels are forest and how much are agricultural areas?
+freq(l2006c$map)
+# agricultural areas and water (class 1) = 35807
+# forest (class 2) = 305485
+
+# calculate the proportion of agricultural and forest areas
+# the number 341292 is the total number of pixel
+# i find this number in the properties of l2006c
+total <- 342726
+propagri2006 <- 163352/total
+propagri2006 #  0.4766256 ~ 0.48 -> agricultural and water areas
+propforest2006 <- 179374/total
+propforest2006 # 0.5233744 ~ 0.52 -> forest areas
+
+# build a dataframe for the 2006 image
+cover <- c("Forest", "Agriculture")
+prop1992 <- c(propforest, propagri)
+prop2006 <- c(propforest2006, propagri2006)
+proportion <- data.frame(cover, prop1992, prop2006)
+
+# let's make a plot, an histogram
+ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white") 
+
+# let's plot everything together
+# we need the library(gridExtra) to use the grid.arrange function to put several graphs in the multi frame
+# let's first assign every ggplot to an object
+p1 <- ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white") 
+p2 <- ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white") 
+
+grid.arrange(p1, p2, nnow=1)
+
+# to have the same scale bar and the same y max value, we can add ylim(0,1) to the ggplot fuunction 
+p1bis <- ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
+p2bis <- ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
+grid.arrange(p1bis, p2bis, nnow=1)?? non va
+
+
+
