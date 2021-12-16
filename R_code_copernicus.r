16538b2f32d7d2afb4537ec7340e87e6a964fe0c
silviarigatti <92572878+silviarigatti@users.noreply.github.com>
2021-12-16 12:16:47 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -14,3 +14,26 @@ snow20211214
 
 # let's plot the image
 plot(snow20211214)
+# the poles have saturation: the amount of snow is high.
+
+# 16.12.2021
+
+cl <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(snow20211214, col=cl)
+
+# use viridis package to change colours, and use colours that can be seen also by blind or daltonic people
+install.packages("viridis")
+library(viridis)
+library(RStoolbox)
+library(ggplot2)
+
+ggplot() + geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent))
+
+#ggplot function with viridis
+
+ggplot() + geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) + 
+scale_fill_viridis() + ggtitle("viridis palette")
+
+# with other colours
+ggplot() + geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) + scale_fill_viridis(option="cividis")
+# instead of typing cividis, you can also type "A", "B", etc., where every letter is a colour set (see online).
