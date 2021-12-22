dc4a05cd7f1f4755cc3602ffa8368336a4a94662
silviarigatti <92572878+silviarigatti@users.noreply.github.com>
2021-12-22 12:43:11 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -1,5 +1,5 @@
 # R code for species distribution modelling, namely the distribution of individuals
-
+# sdm= spacial distribution modelling package
 install.packages("sdm")
 library(sdm)
 # we will use files inside the package sdm
@@ -7,3 +7,14 @@ library(sdm)
 library(raster) # predictors
 library(rgdal) # species: they are points, not raster. we use rgdal for the species analysis
 
+# now, we use the system.file function. It shows all the files in a certain package.
+file <- system.file("external/species.shp", package="sdm")
+# shp is shapefile
+# external is a folder inside the R folder in my pc
+file
+# we obrain "C:/Users/Silvia/Documents/R/win-library/4.1/sdm/external/species.shp": it's the path of the data
+# now we re create a file from the shp file with a shapefile fuunction, to import that shapefile
+# it works exactly as the raster function for raster files. they are points in space
+species <- shapefile(file)
+species
+plot(species, pch=19, col="red") # pch is a type of point, look iin the internet: every number is a type of point
