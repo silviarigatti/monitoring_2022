860212f3750a2d0d62ac54e6725f1695e36d0e18
silviarigatti <92572878+silviarigatti@users.noreply.github.com>
2022-01-10 10:46:49 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -4,7 +4,7 @@ install.packages("sdm")
 library(sdm)
 # we will use files inside the package sdm
 
-library(raster) # predictors
+library(raster) # predictors: they are the environmental variables used to predidcts where the species can be found over space
 library(rgdal) # species: they are points, not raster. we use rgdal for the species analysis
 
 # now, we use the system.file function. It shows all the files in a certain package.
@@ -18,3 +18,28 @@ file
 species <- shapefile(file)
 species
 plot(species, pch=19, col="red") # pch is a type of point, look iin the internet: every number is a type of point
+
+
+#10.01.2022
+
+# how many occurrences are there? Subset a DataFrame
+species[species$Occurrence == 1,]
+
+presences <- species[species$Occurrence == 1,]
+absences <- species[species$Occurrence == 0,]
+
+plot(species, pch=19, col="red") # pch is a type of point, look iin the internet: every number is a type of point
+
+plot(species, pch=19)
+
+plot(presences, pch=19, col="blue")
+
+points(absences, pch=19, col="red")
+
+path <- system.file("external", package="sdm")
+
+# list the predictors
+lst <- list.files(path, pattern='asc')
+
+
+# vedi lezione 
